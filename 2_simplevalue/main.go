b78335b1f3b01d6358221aac2edd666dbc0bf2b0
Anurag Aman <anurag.nitm@gmail.com>
2025-01-01 06:47:39 +0530

    all file added in stagging area

---
+++ 2_simplevalue/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello")
+	fmt.Println("hello" + "world")
+	fmt.Println(3.0 / 2.0)
+
+}
---
+++ 3_variable/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num int = 6
+	var num2 = 9 // agr ap type define nahi kroge to go khud type define kr dega
+
+	var num3 = "9" // yha maine type nahi diya wo automaticaly string define kr dega
+
+	var num4 float64 = 10.8
+
+	num5 := 10.99 // shortest way to initialise and create value
+
+	// But we cant do this short form and without type when we have to initialise variable value later
+
+	var num6 string
+
+	num6 = "Anurag Aman"
+
+	fmt.Println(num)
+	fmt.Println(num2)
+	fmt.Println(num3)
+	fmt.Println(num4)
+	fmt.Println(num5)
+	fmt.Println(num6)
+
+}
