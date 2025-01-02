aee4fa179ba491a7dc4fd5a30e08377000b3476e
Anurag Aman <anurag.nitm@gmail.com>
2025-01-02 07:45:39 +0530

    important about variable define outside of function

---
+++ 6_imp_var_knowledge/var_know.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var name string = "Anurag" // yha hum variable bahar vi define kr sakte hain and then function ke andar se usko access kr sakte hain
+
+var name1 = "Shivam"
+
+// name2:="Hrash" ---> ye short form bs function ke andar chalega baki sab bahar chalega
+
+func main() {
+	fmt.Println(name)
+	fmt.Println(name1)
+
+}
