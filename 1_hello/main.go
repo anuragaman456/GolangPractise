3dba6b7cf67169a3cceb104e3f8cf12cb96c5bab
Anurag Aman <anurag.nitm@gmail.com>
2025-01-04 23:40:31 +0530

    Update main.go

--- 1_hello/main.go
+++ 1_hello/main.go
@@ -1,10 +1,10 @@
-package main // basically it tell that it is entry of the go file and without this u cant able to make executable file
+package main 
 
-import "fmt" //The import statement brings in external libraries or packages into your code. Here, "fmt" isa standard Go library used for formatted I/O (like printing to the console)
+import "fmt"
 
-func main() { // The Go runtime requires a main function in the main package to begin execution. It’s where the program logic is implemented.
-	fmt.Println("Hello World3") // from external fmt package it is importing println to console the output in cli
+func main() {
+	fmt.Println("Hello World3") 
 
 }
 
-// two ways of execution -{1)go build main.go then ./main.exe   2) use directly go run main.go}
+
